c0f643a94ff9348d7d277e4bda3e7712b7862b48
Rohan Verma <rohanv@BLADE51.redmond.corp.microsoft.com>
2018-12-03 22:13:50 +0530

    added get todo and post todo route

--- server/server.js
+++ server/server.js
@@ -2,35 +2,46 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 
-var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo')
-var {User} = require('./models/user')
+var {
+    mongoose
+} = require('./db/mongoose');
+var {
+    Todo
+} = require('./models/todo')
+var {
+    User
+} = require('./models/user')
 
 var app = express();
 
 app.use(bodyParser.json());
 
-app.post('/todos', (req,res) => {
-    console.log(req);
+app.post('/todos', (req, res) => {
 
-    var todo = new Todo( {
-        text: req.body.text
-    });
+var todo = new Todo({
+    text: req.body.text
+});
 
-    todo.save().then((doc) => {
-        res.send(doc);
-    }, (e) => {
-        res.send(e);
+todo.save().then((todo) => {
+    res.status(200).send(todo);
+}, (e) => {
+        res.status(400).send(e);
     });
-    
 });
 
 
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.status(200).send({
+            todos
+        })
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
 
 
 app.listen(3000, () => {
     console.log('server is up on port 3000');
-    
+
 })
-
-
